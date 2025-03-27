6efd4cc8b1370d2692c9ae6c97127c1a967f8e53
hy5528 <95360525+hy5528@users.noreply.github.com>
2025-03-27 12:57:41 +0800

    Update lede.sh

--- sh/lede.sh
+++ sh/lede.sh
@@ -57,14 +57,7 @@ merge_package main https://github.com/xiaorouji/openwrt-passwall2 package/app lu
 merge_package main https://github.com/xiaorouji/openwrt-passwall package/app luci-app-passwall
 merge_package dev https://github.com/vernesong/OpenClash package/app luci-app-openclash
 
-# 内核，参照 kiddin9
-rm -rf target/linux/amlogic
-shopt -s extglob
-SHELL_FOLDER=$(dirname $(readlink -f "$0"))
-merge_package main https://github.com/lxiaya/openwrt-onecloud target/linux target/linux/amlogic
-sed -i "s/wpad-openssl/wpad-basic-mbedtls/" target/linux/amlogic/image/Makefile
-# sed -i "s/neon-vfpv4/vfpv4/" target/linux/amlogic/meson8b/target.mk
-rm -rf package/feeds/routing/batman-adv
+
 # 添加无线网卡支持,似乎不起作用？
 # sed -i '/bool "Enable SDIO bus interface support"/a\		default y if TARGET_amlogic' package/kernel/mac80211/broadcom.mk
 
